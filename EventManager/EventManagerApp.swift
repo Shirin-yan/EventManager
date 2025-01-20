bbb9c800431e7d2171700435d8e4e65b6e75be4e
Shirin-Yan <shirinayangibayeva@gmail.com>
2025-01-20 16:22:11 +0500

    project created

---
+++ EventManager/EventManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventManagerApp.swift
+//  EventManager
+//
+//  Created by Shirin-Yan on 20.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct EventManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
